a918de67b3fa0cb34a066c35cfaf2d800c15d6d4
programmeramit <kamit896837@outlook.com>
2025-01-26 09:52:33 +0530

    added

--- example/views.py
+++ example/views.py
@@ -2,15 +2,8 @@
 from datetime import datetime
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello this is amit</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    return render(request,"index.html")
